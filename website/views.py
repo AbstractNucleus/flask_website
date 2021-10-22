6ffeb7da45d0d47458908ef69ad1cc346716f741
Noel Kleen <noelhkleen@gmail.com>
2021-10-22 23:05:07 +0200

    finished rsa

--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, get_flashed_messages, render_template, flash
 from flask.globals import request
 import os, sys
+
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
-import backend.projects.rsa.rsa_modules as rsa
 
 views = Blueprint("views", __name__)
 
@@ -17,7 +17,7 @@ def home():
 def about():
     return render_template("about.html")
 
-@views.route("/project")
+@views.route("/projects/")
 @views.route("/projects")
 def projects():
     return render_template("projects.html")
@@ -25,12 +25,26 @@ def projects():
 @views.route("/rsa", methods=['GET', 'POST'])
 @views.route("/projects/rsa", methods=['GET', 'POST'])
 def rsa():
-    if request.method == "POST":
-        message = int(request.form.get("message"))
-        n_encrypt = int(request.form.get("n_encrypt"))
-        e_encrypt = int(request.form.get("e_encrypt"))
-        cipher = rsa.encrypt(message, n_encrypt, e_encrypt)
-        flash(cipher)
-
+    import backend.projects.rsa.rsa_modules as rsa
     p, q, F, n, e, d = rsa.key_gen(512)
+    if request.method == "POST":
+        if request.form.get("message") and request.form.get("n_encrypt") and request.form.get("e_encrypt"):
+            input_message = int(request.form.get("message"))
+            n_encrypt = int(request.form.get("n_encrypt"))
+            e_encrypt = int(request.form.get("e_encrypt"))
+            cipher = rsa.encrypt(input_message, n_encrypt, e_encrypt)
+            return render_template("rsa.html", p=p, q=q, F=F, n=n, 
+                               e=e, d=d, cipher=cipher, n_encrypt=n_encrypt, 
+                               e_encrypt=e_encrypt, n_decrypt=0, 
+                               d_decrypt=0, output_message=0)
+        if request.form.get("input_cipher"):
+            input_cipher = int(request.form.get("input_cipher"))
+            n_decrypt = int(request.form.get("n_decrypt"))
+            d_decrypt = int(request.form.get("d_decrypt"))
+            output_message = rsa.decrypt(input_cipher, n_decrypt, d_decrypt)
+            return render_template("rsa.html", p=p, q=q, F=F, n=n, 
+                               e=e, d=d, cipher=0, n_encrypt=0, 
+                               e_encrypt=0, n_decrypt=n_decrypt, 
+                               d_decrypt=d_decrypt, output_message=output_message)
+    
     return render_template("rsa.html", p=p, q=q, F=F, n=n, e=e, d=d)
