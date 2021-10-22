ac7b1b3164663c01fa7c8bf8f645c36263d6a3d5
AbstractNucleus <noelhkleen@gmail.com>
2021-10-22 15:10:07 +0200

    rsa almost finished

--- website/views.py
+++ website/views.py
@@ -1,4 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
+from flask.globals import request
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+import backend.projects.rsa.rsa_modules as rsa
 
 views = Blueprint("views", __name__)
 
@@ -16,15 +22,15 @@ def about():
 def projects():
     return render_template("projects.html")
 
-@views.route("/rsa")
-@views.route("/projects/rsa")
+@views.route("/rsa", methods=['GET', 'POST'])
+@views.route("/projects/rsa", methods=['GET', 'POST'])
 def rsa():
-    import os, sys
-    currentdir = os.path.dirname(os.path.realpath(__file__))
-    parentdir = os.path.dirname(currentdir)
-    sys.path.append(parentdir)
-    from backend.projects.rsa.rsa_modules import key_gen
-    
-    p, q, F, n, e, d = key_gen(512)
-    
+    if request.method == "POST":
+        message = int(request.form.get("message"))
+        n_encrypt = int(request.form.get("n_encrypt"))
+        e_encrypt = int(request.form.get("e_encrypt"))
+        cipher = rsa.encrypt(message, n_encrypt, e_encrypt)
+        flash(cipher)
+
+    p, q, F, n, e, d = rsa.key_gen(512)
     return render_template("rsa.html", p=p, q=q, F=F, n=n, e=e, d=d)
